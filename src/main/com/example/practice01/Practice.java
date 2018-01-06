3b493cae52dfaa46ca89593b18be3e8c1ac7123c
Albert Wong <albertw@ieee.org>
2018-01-05 17:39:09 -0800

    Initial

---
+++ src/main/com/example/practice01/Practice.java
@@ -0,0 +1,7 @@
+package com.example.practice01;
+
+public class Practice {
+    public static void main(String[] args) {
+        System.out.println("Hello! Practice01");
+    }
+}
---
+++ src/test/com/example/practice01/PracticeTest.java
@@ -0,0 +1,11 @@
+package com.example.practice01;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PracticeTest {
+    @BeforeEach
+    void setUp() {
+    }
+}
